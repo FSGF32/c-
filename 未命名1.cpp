47d031c2db466d7f065e208b24c8c3d347f45443
FSGF32 <fsgfsmy@outlook.com>
2024-12-07 13:54:16 +0800

    Add files via upload
    身高体重计算器

---
+++ 未命名1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    long long n;
+    long long q;
+    cout<<"输入你的身高和体重 ";
+	cin>>n>>q;
+	cout<<"身高和体重分别是 ";
+	cout<<n;
+    cout<<"  ";
+	cout<<q;
+    return 0;
+}
